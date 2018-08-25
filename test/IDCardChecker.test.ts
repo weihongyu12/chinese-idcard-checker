67eaaa1616960b63603481df2aa8dca9c5ba0032
weihongyu12 <weihongyu12@outlook.com>
2018-08-25 19:03:18 +0800

    update test

--- test/IDCardChecker.test.ts
+++ test/IDCardChecker.test.ts
@@ -27,14 +27,34 @@ describe('验证中国居民身份证', () => {
     expect(result).to.equal('北京市');
   });
 
+  it('获取省份（身份证号错误）', () => {
+    const result = IDCardChecker.getProvince('160105194912310029');
+    expect(result).to.equal(null);
+  });
+
   it('获取出生日期', () => {
     // @ts-ignore
     const result = IDCardChecker.getBirthDate('11010519491231002X').getTime();
     expect(result).to.equal(new Date('1949-12-31').getTime());
   });
 
-  it('获取性别', () => {
+  it('获取出生日期（身份证号错误）', () => {
+    const result = IDCardChecker.getBirthDate('110105194902310026');
+    expect(result).to.equal(null);
+  });
+
+  it('获取性别（女）', () => {
     const result = IDCardChecker.getGender('11010519491231002X');
     expect(result).to.equal('女');
+  });
+
+  it('获取性别（男）', () => {
+    const result = IDCardChecker.getGender('350724199111282910');
+    expect(result).to.equal('男');
+  });
+
+  it('获取性别（身份证号错误）', () => {
+    const result = IDCardChecker.getGender('110105194912310021');
+    expect(result).to.equal(null);
   });
 });
