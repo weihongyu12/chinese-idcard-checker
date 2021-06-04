e19e4a61b63dd8fce4789cc4b58cfc78566b58b3
weihongyu12 <weihongyu12@outlook.com>
2021-06-04 20:47:20 +0800

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,17 @@
-import IDCardChecker from '../src/IDCardChecker';
+export type ValidateHandle = (idCardNum: string) => boolean;
 
-export default IDCardChecker;
+export type BirthDateHandle = (idCardNum: string) => Date | null;
+
+type Gender = '男' | '女';
+
+export type GenderHandle = (idCardNum: string) => Gender | null;
+
+export type ProvinceHandle = (idCardNum: string) => string | null;
+
+export default interface IDCardChecker {
+  pattern: RegExp;
+  validate: ValidateHandle;
+  birthDate: BirthDateHandle;
+  gender: GenderHandle;
+  province: ProvinceHandle;
+}
